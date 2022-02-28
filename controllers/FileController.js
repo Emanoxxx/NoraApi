141b9389cc61a583c6c661b253a49e9a7db04376
emanoxxx <emanohalowar@hotmail.com>
2022-02-28 15:30:35 -0600

    cambios

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -1,12 +1,20 @@
+const fleSystem = require('fs');
 class FileController
 {
   subirArchivo = async (req, res, next) =>
   {
-    const archivo = req.files;
+    const
+  }
+}
+async function makeFile(req){
+  const archivo = req.files;
     const fileName = archivo["audio0"].name;
-    const path = __dirname + '/../uploads/' + fileName;
+    const path = __dirname + '/../Sounds/'+req.body.nombre +"/"+ fileName;
+    console.log(archivo["audio0"]);
+    const carpeta=__dirname + '/../Sounds/'+req.body.nombre +"/";
+    await makeCarpeta(carpeta);
     try {
-      archivo.mv(path, (error) => {
+      const file = await fleSystem.writeFile(path,archivo["audio0"].data, (error) => {
         if (error) {
           console.error(error);
           res.writeHead(500, {
@@ -23,7 +31,32 @@ class FileController
          message: e.toString()
        });
      }
+}
+async function makeCarpeta(carpeta){
+  try {
+      if (!fileExists(carpeta)) {
+        console.log("no exists: " + carpeta);
+         await fleSystem.mkdirSync(carpeta,{recursive:true},(error) => {
+          if (error) {
+            console.error(error);
+            res.writeHead(500, {
+              'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify({ status: 'error', message: error }));
+              return;
+            }
+         });
+      }
+    } catch (error) {
+      
+    }
+    return;
+}
+function fileExists(path) {
+  try {
+    return fs.statSync(path).isFile();
+  } catch (e) {
+    return false;
   }
 }
-
 module.exports = FileController;
