17e033e7904f9187d36404722a7ffea4ed35465a
emanoxxx <emanohalowar@hotmail.com>
2022-03-24 22:28:38 -0600

    registro

--- Models/dao.js
+++ Models/dao.js
@@ -129,6 +129,13 @@ const obtenerArchivo = async(sonido) => {
     $query = 'select path,url from Sonido,UrlSonido,(select sonido from EtiquetaSonido where etiqueta='+connection.escape(sonido)+')as etiqueta where nombre=etiqueta.sonido and UrlSonido.sonido=etiqueta.sonido ORDER BY RAND() LIMIT 1;';
     try{return await query($query);}catch(error){return error}
 };
+//Registrar usuario
+const createUsuario = async(usuario) => {
+    var resultado=null;
+    // Realizar una consulta
+    $query = 'insert into Usuario(nombre,usuario,pass,isActive,email) values('+connection.escape(usuario.nombre)+','+connection.escape(usuario.usuario)+','+connection.escape(usuario.pass)+',false,'+connection.escape(usuario.correo)+')'
+    try{return await query($query);}catch(error){return error}
+};
 //Tabla sonido
 exports.getSearch=getSearch;
 exports.getSonidoByName=getSonidoByName;
@@ -159,6 +166,7 @@ const getLogin = async(user) => {
     //return resultado;
 };
 //Tabla Usuario
+exports.createUsuario=createUsuario;
 exports.getLogin=getLogin;
 //Obtener ruta Archivo
 exports.obtenerArchivo=obtenerArchivo;
